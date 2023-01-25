777f0002a8c32c2230a74206ae97c7d366e72853
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-25 13:37:25 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "forwards") {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, value)
+    } else if (name == "back") {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, value)
+    } else if (name == "left") {
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, value / 2)
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, value)
+    } else if (name == "right") {
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, value / 2)
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, value)
+    } else {
+        Kitronik_Move_Motor.stop()
+    }
+})
+radio.setGroup(255)
